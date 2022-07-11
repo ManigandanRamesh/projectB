cc3b3790c7f105bdb62ec1e5c5d8aa6c78d3a07e
ManigandanRamesh <105410116+ManigandanRamesh@users.noreply.github.com>
2022-07-11 16:58:39 +0530

    upload new files

---
+++ projectB/src/com/chainsys/application/ProjectBApplication.java
@@ -0,0 +1,13 @@
+package com.chainsys.application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class ProjectBApplication {
+
+	public static void main(String[] args) {
+   SpringApplication.run(ProjectBApplication.class, args);
+	}
+	
+} 
